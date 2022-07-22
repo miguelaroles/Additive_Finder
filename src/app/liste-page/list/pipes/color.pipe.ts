51a35567a1c9530d04037c8fc73ba90126aa097a
Miguel Aroles <miguelaroles@MacBook-Pro-de-Miguel.local>
2022-07-22 17:16:53 +0200

    FIX Pipes

--- src/app/liste-page/list/pipes/color.pipe.ts
+++ src/app/liste-page/list/pipes/color.pipe.ts
@@ -1,17 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-enum Code {
-  success = 'success',
-  tertiary = 'tertiary',
-  warning = 'warning',
-  danger = 'danger'
-}
+const Legend: string[] = [
+  'success',
+  'tertiary',
+  'warning',
+  'danger'];
 
 @Pipe({
   name: 'color'
 })
 export class ColorPipe implements PipeTransform {
   transform(value: string): string {
-    return Code[value as keyof typeof Code] || 'default';
+    return Legend[Number(value)] || 'default';
   }
 }
